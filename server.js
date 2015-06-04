f27cfbdc855ed75322a343f8a9ae4c6e946a58f2
Yoshua Wuyts <i@yoshuawuyts.com>
2015-06-04 11:40:11 +0200

    server: handle CORS

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require('http')
 
 const server = http.createServer(function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'file://')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   res.setHeader('Set-Cookie', ['type=ninja', 'language=javascript'])
   res.end('whoop!')
 })
