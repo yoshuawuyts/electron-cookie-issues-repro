e775d70371ed3379e5efce49afd4eaa43266b276
Yoshua Wuyts <i@yoshuawuyts.com>
2015-06-08 11:29:33 +0200

    repro: rm unneeded files

--- client.js
+++ client.js
@@ -1,30 +1,15 @@
 require('electron-cookies')
 const nets = require('nets')
 
-// window.fetch does not support
-// cookies.
-window.fetch('http://localhost:1337')
-  .then(function (res) {
-    console.log('res', res)
-    return res.text()
-  })
-  .then(function (body) {
-    console.log('body', body)
-    console.log('cookies', document.cookies)
-    callNets()
-  })
-
 // `withCredentials` set to true
 // allows cookies to be set
-function callNets () {
-  nets({
-    url: 'http://localhost:1337',
-    encoding: null,
-    withCredentials: true
-  }, function (err, res, body) {
-    if (err) return console.error(err)
-    console.log('res', res)
-    console.log('body', body)
-    console.log('cookies', document.cookies)
-  })
-}
+nets({
+  url: 'http://localhost:1337',
+  encoding: null,
+  withCredentials: true
+}, function (err, res, body) {
+  if (err) return console.error(err)
+  console.log('res', res)
+  console.log('body', body)
+  console.log('cookies', res.headers['set-cookie'])
+})
